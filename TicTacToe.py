7c431ddb5735bd12e69e1b5044068fc7b8b55293
Pasindu <pasindusandakan@gmail.com>
2023-11-29 11:34:23 +0530

    method for print board

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,1 +1,15 @@
-print(1)
+board = ['-'] * 9
+winner = None
+current_player = 'X'
+game_running = True
+
+
+def print_board(board):
+    print(board[0] + '|' + board[1] + '|' + board[2])
+    print('-----')
+    print(board[3] + '|' + board[4] + '|' + board[5])
+    print('-----')
+    print(board[6] + '|' + board[7] + '|' + board[8])
+
+
+print_board(board)
