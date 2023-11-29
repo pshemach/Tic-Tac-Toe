25e1eaa86db55d53b5cdf9aec0edf23be3e9739b
Pasindu <pasindusandakan@gmail.com>
2023-11-29 19:25:13 +0530

    crated game loop

--- TicTacToe.py
+++ TicTacToe.py
@@ -38,7 +38,78 @@ def compute(board):
             switch_player(current_player)
 
 
-player_input(board)
-switch_player(current_player)
-compute(board)
-print_board(board)
+def check_column(board):
+    global winner
+    if board[0] == board[1] == board[2] and board[0] != '-':
+        winner = board[0]
+        return True
+    elif board[3] == board[4] == board[5] and board[3] != '-':
+        winner = board[3]
+        return True
+    elif board[6] == board[7] == board[8] and board[6] != '-':
+        winner = board[6]
+        return True
+
+
+def check_raw(board):
+    global winner
+    if board[0] == board[3] == board[6] and board[0] != '-':
+        winner = board[0]
+        return True
+    elif board[1] == board[4] == board[7] and board[1] != '-':
+        winner = board[1]
+        return True
+    elif board[2] == board[5] == board[8] and board[2] != '-':
+        winner = board[2]
+        return True
+
+
+def check_diagonal(board):
+    global winner
+    if board[0] == board[4] == board[8] and board[0] != '-':
+        winner = board[0]
+        return True
+    elif board[2] == board[4] == board[7] and board[6] != '-':
+        winner = board[2]
+        return True
+
+
+def check_win(board):
+    global game_running
+    if check_column(board):
+        print_board(board)
+        print(f'The Winner: {winner}')
+        game_running = False
+    elif check_raw(board):
+        print_board(board)
+        print(f'The Winner: {winner}')
+        game_running = False
+    elif check_diagonal(board):
+        print_board(board)
+        print(f'The Winner: {winner}')
+        game_running = False
+
+
+def check_tie(board):
+    global game_running
+    if '-' not in board:
+        print_board(board)
+        print("It's Tie")
+        game_running = False
+
+
+while game_running:
+    print_board(board)
+    player_input(board)
+    check_win(board)
+    if not game_running:
+        break
+    check_tie(board)
+    if not game_running:
+        break
+    switch_player(current_player)
+    compute(board)
+    check_win(board)
+    if not game_running:
+        break
+    check_tie(board)
