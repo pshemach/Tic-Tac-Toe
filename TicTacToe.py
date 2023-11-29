86fcd201493a0697e46ecb8a913a516be25a363b
Pasindu <pasindusandakan@gmail.com>
2023-11-29 19:00:54 +0530

    method for switch players

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,3 +1,5 @@
+import random
+
 board = ['-'] * 9
 winner = None
 current_player = 'X'
@@ -14,9 +16,29 @@ def print_board(board):
 
 def player_input(board):
     inp = int(input('Enter a Number (1-9):'))
-    if board[inp-1]=='-':
-        board[inp-1] = current_player
+    if board[inp - 1] == '-':
+        board[inp - 1] = current_player
     else:
         print('Oops Space is occupied..!!!')
 
 
+def switch_player(c_player):
+    global current_player
+    if c_player == 'X':
+        current_player = 'O'
+    else:
+        current_player = 'X'
+
+
+def compute(board):
+    while current_player == 'O':
+        position = random.randint(0, 8)
+        if board[position] == '-':
+            board[position] = 'O'
+            switch_player(current_player)
+
+
+player_input(board)
+switch_player(current_player)
+compute(board)
+print_board(board)
